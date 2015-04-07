abff50fc1cb2f790cf6c317228a5435ecdfc6a60
Howard Tyson <howard@howardtyson.com>
2015-04-07 15:39:08 -0400

    Adding tests and refactoring to use through2 streams.

--- index.js
+++ index.js
@@ -5,21 +5,37 @@ var LogDB = function(db, options) {
   this.db = db;
   LevelWriteStream(this.db);
   this.options = options || {};
+  this.options.padding = this.options.padding || '0000000';
+  this.options.paddingLength = this.options.padding.length;
   this.storeLogFile = this.storeLogFile.bind(this);
 };
 
+/**
+ * Stores a record for the log file and returns the write stream used to append lines to it.
+ */
 LogDB.prototype.storeLogFile = function(filename, options, done) {
   if (done == undefined) {
     done = options;
     options = {};
   }
-  var logStream = this.createLogWriteStream(filename, options);
-  var dbWriteStream = this.db.createWriteStream();
-  dbWriteStream.on('finish', function() {
-    logStream.emit('finish');
+  var prefix = options.prefix || 'logdb';
+  var separator = options.separator || '!';
+  var value = options.jobData || {
+    date: Date.now(),
+  };
+  var self = this;
+  var key = prefix + separator + Date.now() + separator + filename;
+  this.db.put(key, value, { keyEncoding: 'json' }, function(error) {
+    if (error) return done(error);
+    var logStream = self.createLogWriteStream(filename, options);
+    var dbWriteStream = self.db.createWriteStream();
+    logStream.pipe(dbWriteStream);
+    done(null, logStream);
   });
-  logStream.pipe(dbWriteStream);
-  done(null, logStream);
+};
+
+LogDB.prototype.getPaddedTimestamp = function(number) {
+  String(this.options.padding + number).slice(this.options.paddingLength);
 };
 
 /**
@@ -35,10 +51,12 @@ LogDB.prototype.createLogWriteStream = function(filename, options) {
   var line = 0;
   var prefix = options.prefix || 'logdb';
   var separator = options.separator || '!';
+  var valueEncoding = options.valueEncoding || 'utf8';
   return through2.obj(function(data, enc, done) {
     if (data == '') return;
     var element = {
       key: prefix + separator + filename + separator + separator + line,
+      valueEncoding: valueEncoding,
       value: data,
       type: 'put',
     };
@@ -50,15 +68,20 @@ LogDB.prototype.createLogWriteStream = function(filename, options) {
 
 /**
  * Streams the contents of a file out.
+ *
+ * @param filename
+ *   The name of the log stream.
  */
 LogDB.prototype.readLogFile = function(filename, options) {
   options = options || this.options;
   var prefix = options.prefix || 'logdb';
   var separator = options.separator || '!';
+  var valueEncoding = options.valueEncoding || 'utf8';
   var baseKey = prefix + separator + filename + separator + separator;
   var config = {
     start: baseKey + '!',
     end: baseKey + '~',
+    valueEncoding: valueEncoding,
   };
   return this.db.createValueStream(config);
 }
--- test/log-test.js
+++ test/log-test.js
@@ -1,4 +1,5 @@
 var should = require('should');
+var through2 = require('through2');
 var es = require('event-stream');
 var fs = require('fs');
 var level = require('level')
@@ -11,9 +12,12 @@ describe('LogDB', function() {
   before(function(done) {
     db = level('/tmp/logdb-' + Date.now(), done);
   });
-  it('should store a log and retrieve the contents of a log file', function(done) {
+  it('should store a record of the log file', function(done) {
+    done();
+  });
+  it('should store and retrieve a text stream for a log file in order', function(done) {
     var logdb = new LogDB(db);
-    logdb.storeLogFile('test.log', function(error, logStream) {
+    logdb.storeLogFile('test1.log', function(error, logStream) {
       var source = fs.createReadStream(__dirname + '/fixtures/example1.log');
       var split = source.pipe(es.split());
       split.pipe(logStream);
@@ -24,7 +28,7 @@ describe('LogDB', function() {
           }
         }))
         .pipe(es.writeArray(function(error, writtenData) {
-        var readStream = logdb.readLogFile('test.log');
+        var readStream = logdb.readLogFile('test1.log');
         // For better or worse, I can't figure out how to be sure this stuff will
         // come back without just waiting a bit.
         setTimeout(function() {
@@ -34,6 +38,34 @@ describe('LogDB', function() {
           }));
         }, 5);
       }));
+    });
+  });
+  it('should store a json stream for a log file in order', function(done) {
+    var logdb = new LogDB(db);
+    logdb.storeLogFile('test2.log', { valueEncoding: 'json' }, function(error, logStream) {
+      var writtenData = [];
+      var source = fs.createReadStream(__dirname + '/fixtures/example1.log')
+        .pipe(es.split())
+        .pipe(through2.obj(function(chunk, enc, done) {
+          var item = {
+            foo: 'bar',
+            data: chunk.toString('utf8'),
+          };
+          this.push(item);
+          writtenData.push(item);
+          done();
+        }));
+      source.pipe(logStream);
+      source.on('end', function() {
+        setTimeout(function() {
+          var readStream = logdb.readLogFile('test2.log', { valueEncoding: 'json' });
+          readStream.pipe(es.writeArray(function(error, readData) {
+            // Is there a better way to compare values of arrays of objects?
+            JSON.stringify(readData).should.equal(JSON.stringify(writtenData));
+            done();
+          }));
+        }, 5);
+      });
     });
   });
 });
