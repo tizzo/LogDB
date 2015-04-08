b214747c74353060d8873bf83cc46af260c48c97
Howard Tyson <howard@howardtyson.com>
2015-04-07 21:30:21 -0400

    Switching to use getPaddedNumber in building keys, adding comments.

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 var through2 = require('through2');
 var LevelWriteStream = require('level-writestream');
 
+/**
+ * LogDB Constructor.
+ *
+ * @param db
+ *   A LevelUp compatible database.
+ * @param options
+ *   An array of options including:
+ *     prefix: key prefix
+ *     separator: separates key peices
+ *     padding: pad the line number integers
+ *     keyEncoding: encoding for the keys.
+ *     valueEncoding: encoding for the values.
+ */
 var LogDB = function(db, options) {
   this.db = db;
   LevelWriteStream(this.db);
@@ -8,16 +21,23 @@ var LogDB = function(db, options) {
   this.options.padding = this.options.padding || '0000000';
   this.options.paddingLength = this.options.padding.length;
   this.storeLogFile = this.storeLogFile.bind(this);
+  this.getPaddedNumber = this.getPaddedNumber.bind(this);
 };
 
 /**
  * Stores a record for the log file and returns the write stream used to append lines to it.
+ *
+ * @param filename
+ *   The name of the log.
+ * @param options
+ *   An optinal array of options
  */
 LogDB.prototype.storeLogFile = function(filename, options, done) {
   if (done == undefined) {
     done = options;
-    options = {};
+    options = this.options;
   }
+  options = options || this.options;
   var prefix = options.prefix || 'logdb';
   var separator = options.separator || '!';
   var value = options.jobData || {
@@ -26,16 +46,23 @@ LogDB.prototype.storeLogFile = function(filename, options, done) {
   var self = this;
   var key = prefix + separator + Date.now() + separator + filename;
   this.db.put(key, value, { keyEncoding: 'json' }, function(error) {
-    if (error) return done(error);
     var logStream = self.createLogWriteStream(filename, options);
+    console.log(self.db);
     var dbWriteStream = self.db.createWriteStream();
+    if (error) return done(error);
     logStream.pipe(dbWriteStream);
-    done(null, logStream);
+    done(error, logStream);
   });
 };
 
-LogDB.prototype.getPaddedTimestamp = function(number) {
-  String(this.options.padding + number).slice(this.options.paddingLength);
+/**
+ * Returns an integer padded with leading zeros to create a predictable key length.
+ *
+ * @parma number
+ *   The number to pad.
+ */
+LogDB.prototype.getPaddedNumber = function(number) {
+  return String(this.options.padding + number).substr(String(number).length, this.options.padding.length);
 };
 
 /**
@@ -52,10 +79,11 @@ LogDB.prototype.createLogWriteStream = function(filename, options) {
   var prefix = options.prefix || 'logdb';
   var separator = options.separator || '!';
   var valueEncoding = options.valueEncoding || 'utf8';
+  var self = this;
   return through2.obj(function(data, enc, done) {
     if (data == '') return;
     var element = {
-      key: prefix + separator + filename + separator + separator + line,
+      key: prefix + separator + filename + separator + separator + self.getPaddedNumber(line),
       valueEncoding: valueEncoding,
       value: data,
       type: 'put',
@@ -71,6 +99,8 @@ LogDB.prototype.createLogWriteStream = function(filename, options) {
  *
  * @param filename
  *   The name of the log stream.
+ * @param options
+ *   An array of options.
  */
 LogDB.prototype.readLogFile = function(filename, options) {
   options = options || this.options;
